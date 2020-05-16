88d546f39036366d03675ece878f0b9986cee7b7
niyarin <inoanet@gmail.com>
2020-05-16 22:00:19 +0900

    Fix behavior of first-commit.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/sh -l
 
-git fetch --depth 1  origin $4
-FILES=`git diff HEAD FETCH_HEAD --name-only|grep '\.clj$'|sed 's/^.*$/"&"/g'|tr "\n" " "`
+BRANCH_NAME=${GITHUB_REF#refs/heads/}
+
+if [ "$4" != "0000000000000000000000000000000000000000" ]
+then
+    git fetch --depth 1  origin $4
+    FILES=`git diff HEAD FETCH_HEAD --diff-filter=AM --name-only|grep '\.clj$'|sed 's/^.*$/"&"/g'|tr "\n" " "`
+elif [ "$BRANCH_NAME" = "master" ]
+then
+    git fetch --unshallow
+    FILES=`git diff 4b825dc642cb6eb9a060e54bf8d69288fbee4904..HEAD --name-only|grep '\.clj$'|sed 's/^.*$/"&"/g'|tr "\n" " "`
+else
+    git fetch --unshallow
+    BASE_NEXT_HASH=$(git log $BRANCH_NAME  --not `git for-each-ref --format='%(refname)' refs/|grep -v $BRANCH_NAME`   --pretty=format:"%H"|tail -n 1)
+
+    FILES=`git diff HEAD $BASE_NEXT_HASH^ --diff-filter=AM --name-only|grep '\.clj$'|sed 's/^.*$/"&"/g'|tr "\n" " "`
+fi
+
 
 cd /lint-action-clj
 
